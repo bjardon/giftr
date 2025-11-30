67c90fa59bb6ba7b9480a6910910a0984ce23c31
Bruno Jardon <5511010+bjardon@users.noreply.github.com>
2025-11-29 22:08:04 -0600

    chore(functions/webhooks): removed test endpoints from webhooks service

--- packages/functions/src/webhooks/index.ts
+++ packages/functions/src/webhooks/index.ts
@@ -4,23 +4,6 @@ import { handle } from "hono/aws-lambda";
 import clerkWebhook from "./clerk";
 
 const app = new Hono().basePath("/webhooks");
-
-app.get("/ping", async (c) => {
-  return c.json({ message: "pong" });
-});
-
 app.route("/clerk", clerkWebhook);
-
-// app.all("*", async (c) => {
-//   return c.json(
-//     {
-//       message: "Catch-all route",
-//       path: c.req.path,
-//       method: c.req.method,
-//       url: c.req.url,
-//     },
-//     404
-//   );
-// });
 
 export const handler = handle(app);
