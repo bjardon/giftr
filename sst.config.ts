6d4dd7a6292cc3cf46a0c35809ea76c3e9d16aec
Bruno Jardon <5511010+bjardon@users.noreply.github.com>
2025-12-04 22:32:41 -0600

    refactor(infra): relocate sst constructs to root sst.config.ts

--- sst.config.ts
+++ sst.config.ts
@@ -7,6 +7,10 @@ export default $config({
       protect: ["production"].includes(input?.stage),
       home: "aws",
       providers: {
+        aws: {
+          //   accessKey: process.env.AWS_ACCESS_KEY_ID!,
+          //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        },
         neon: "0.9.0",
         vercel: {
           version: "3.15.1",
@@ -17,25 +21,107 @@ export default $config({
     };
   },
   async run() {
-    // Core Services
-    const { configureDatabase } = await import("./packages/core/sst.config");
-    const { database } = configureDatabase();
+    const stage = $app.stage;
+    const path = await import("path");
+    const projectRootPath = path.join(__dirname, "../.."); // Relative to .sst/platform (WTF?)
 
-    // API Gateway
-    const { configureFunctions } = await import(
-      "./packages/functions/sst.config"
-    );
-    const { api } = configureFunctions(database);
+    // CORE SERVICES
+    // #region Core Services
 
-    // Web
-    const { configureWeb } = await import("./packages/web/sst.config");
-    const { webProject, webDeployment } = await configureWeb(database);
+    // - Neon Database
+    const autoscalingLimitMinCu = stage === "production" ? 1 : 0.25;
+    const autoscalingLimitMaxCu = stage === "production" ? 2 : 0.25;
+
+    const neonProject = new neon.Project(`NeonProject`, {
+      name: `giftr-${stage}`,
+      pgVersion: 17,
+      regionId: "aws-us-east-1",
+      orgId: process.env.NEON_ORG_ID!,
+      historyRetentionSeconds: 21600,
+      branch: {
+        name: stage,
+        databaseName: `giftr-${stage}-db`,
+      },
+      defaultEndpointSettings: {
+        autoscalingLimitMinCu,
+        autoscalingLimitMaxCu,
+      },
+    });
+
+    const database = new sst.Linkable(`Database`, {
+      properties: {
+        connectionString: neonProject.connectionUri,
+      },
+    });
+
+    // #endregion Core Services
+
+    // API GATEWAY
+    // #region API Gateway
+
+    const api = new sst.aws.ApiGatewayV2(`ApiGateway`, {
+      domain: `api.${process.env.DOMAIN!}`,
+    });
+
+    // - Webhooks Service
+    const webhooksRoute = api.route("ANY /webhooks/{proxy+}", {
+      handler: "packages/functions/src/webhooks/index.handler",
+      link: [database],
+      environment: {
+        DATABASE_URL: database.properties.connectionString,
+        CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET!,
+      },
+    });
+    // #endregion API Gateway
+
+    // WEB APP
+    // #region Web App
+    const webProject = new vercel.Project(`giftr-${stage}-site`, {
+      name: `giftr-${stage}-site`,
+      framework: "nextjs",
+      rootDirectory: "packages/web",
+    });
+
+    const webDomain = new vercel.ProjectDomain(`giftr-${stage}-web-domain`, {
+      projectId: webProject.id,
+      domain: process.env.DOMAIN!,
+    });
+
+    const nextDeploymentFiles = await vercel.getProjectDirectory({
+      path: projectRootPath,
+    });
+
+    const webDeployment = new vercel.Deployment(`giftr-${stage}-deployment`, {
+      projectId: webProject.id,
+      production: true, // Enable production deployment even if stage is not production
+      files: nextDeploymentFiles.files,
+      pathPrefix: projectRootPath, // Strip the leading full path to the project root
+      environment: {
+        CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY!,
+        CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET!,
+        DATABASE_URL: database.properties.connectionString,
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+          process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!,
+      },
+    });
+
+    const webRecord = new aws.route53.Record(`giftr-${stage}-record`, {
+      zoneId: process.env.ROUTE53_ZONE_ID!,
+      name: process.env.DOMAIN!,
+      type: "CNAME",
+      ttl: 300,
+      records: [webDeployment.url],
+    });
+    // #endregion Web App
 
     return {
-      database: database.properties.connectionString,
-      api: api.url,
-      webProject: webProject.urn,
-      webDeployment: webDeployment.url,
+      "database.connectionString": database.properties.connectionString,
+      "api.url": api.url,
+      "api.webhooksRoute": webhooksRoute.urn,
+      "webApp.project": webProject.id,
+      "webApp.deployment": webDeployment.id,
+      "webApp.dnsRecord": webRecord.id,
+      "webApp.url": webDomain.domain,
     };
   },
 });
