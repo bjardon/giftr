0c8adfe1fdec31cc65d27afa102946e479a48a3d
Bruno Jardon <5511010+bjardon@users.noreply.github.com>
2025-12-04 22:33:36 -0600

    refactor(core): update db connection string reference to sst dynamically bind to sst resource depending on environment

--- packages/core/drizzle.config.ts
+++ packages/core/drizzle.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "drizzle-kit";
+import { Resource } from "sst";
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: Resource.Database.connectionString,
   },
 });
--- packages/core/src/db/index.ts
+++ packages/core/src/db/index.ts
@@ -3,9 +3,21 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-const connectionString = process.env.DATABASE_URL!;
+let connectionString: string | undefined;
+
+// Dinamically set the connection string based on the build environment
+//  - Use SST database connectionString if in SST environment (Local or AWS)
+//  - Use DATABASE_URL environment variable if not in SST environment (Vercel)
+if (process.env.SST_STAGE) {
+  const { Resource } = await import("sst");
+  // @ts-ignore -- ignore the type error on non-SST build
+  connectionString = Resource.Database.connectionString;
+} else {
+  connectionString = process.env.DATABASE_URL;
+}
+
 if (!connectionString) {
-  throw new Error("DATABASE_URL is not set");
+  throw new Error("Connection string is not set");
 }
 
 export const sql = neon(connectionString);
